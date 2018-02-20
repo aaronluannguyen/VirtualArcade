d9d76f12e422cae647da5642618bf54e46ea3879
Ken Long <long27km@uw.edu>
2018-02-19 23:09:13 -0800

    working example of loading an arcade machine model from collada file

--- src/App.js
+++ src/App.js
@@ -2,19 +2,78 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {THREE} from './three_wrapper'
+
+var camera, scene, renderer;
+var geometry, material, mesh, loader;
+ var loadingManager;
+init();
+animate();
+ 
+function init() {
+ 
+  loadingManager = new THREE.LoadingManager(()=>scene.add(geometry));
+
+  loadingManager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+
+    console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+    
+    //could update a progress meter the user can see here
+  };
+
+  loadingManager.onError = function ( url ) {
+
+    console.log( 'There was an error loading ' + url );
+  
+    //could display an error the user can see here if one occurs
+  };
+
+    loader = new THREE.ColladaLoader(loadingManager);
+    loader.load('./model/arcade.dae', (collada)=> geometry=collada.scene);
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 2;
+    camera.position.y = 0.25;
+    scene = new THREE.Scene();
+ 
+    //geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    
+    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4);
+    scene.add(ambientLight);
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(1,1,0).normalize();
+
+
+    scene.add(directionalLight);
+
+    //material = new THREE.MeshNormalMaterial();
+ 
+    //mesh = new THREE.Mesh( geometry, material );
+    
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+ 
+}
+ 
+function animate() {
+ 
+    requestAnimationFrame( animate );
+  
+    if(geometry!==undefined)
+    {
+      /*geometry.rotation.x += 0.01;*/
+      /*geometry.rotation.y += 0.02;*/
+      geometry.rotation.z += 0.005;
+    }
+    renderer.render( scene, camera );
+ 
+}
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div id="root"></div>);
   }
 }
 
