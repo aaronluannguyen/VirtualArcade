bb88313836e339e12012ea41f35269bf4313b23c
Kevin Xu <kevinx97@uw.edu>
2018-03-04 15:12:45 -0800

    static dumb implementation with error handling

--- src/views/q20/Q20.js
+++ src/views/q20/Q20.js
@@ -49,7 +49,8 @@ export default class Q20 extends React.Component{
                     questionString: "",
                     labelDetectionData:data.responses[0].labelAnnotations
                 }
-        }));
+        }))
+        .catch(err => this.setState({error: err.message}));
     }
     
     handleResponse(response) {
@@ -101,9 +102,15 @@ export default class Q20 extends React.Component{
     }
 
     render(){
-        
         return (
             <div className="container">
+                {
+                    this.state.error ?
+                        <div className="alert alert-danger mt-3">
+                            {this.state.error}
+                        </div> :
+                        undefined
+                }
                 {
                     this.state.playing ? 
                         <div className="container">
@@ -129,7 +136,7 @@ export default class Q20 extends React.Component{
                         </div> :
                         <div className="container picture">
                             <img className="img-fluid" onClick={() => this.handleStartGame()}
-                                src="https://00e9e64bacbbf177388b67ddfe6a5d3afbff5f5d826472a8c2-apidata.googleusercontent.com/download/storage/v1/b/info343/o/pickle_rick.jpg?qk=AD5uMEsM9wJhMSSl6JXcprUbDQ4axLI-fKYlCTBRGTS-yF_UhsiWn2n4xbU2yDVFy2R1UpdiUQkR_xkWf15N88YjeEMVOT3yCS1aFaIwK5Ebr7qMKHfJk5h6mvaK7x3ACR94U2yx6EZem2zsiPWGLw7Z13gcrycK5DGEnw8nuEoPBDXwTmeOI4ABVQ_q2gcgOYu5dQiFXrmyjEDrNvZu6HP6VCYOkzO-Yx45jQoMX7-5gm0MD2HtMRMvFUq3yiu-KK8FjarfMtdSW1Wk3V_i_ARL86b4H_u_-uuTkXGU3Z_iVvip4MAB-VYw9m2e3xwL-bHLszoT0yLez7h4FxZo9zXxP8wkTst7ss3qplxc8S_Vo01_lvKkvEkpNIE7BfNQzeOIJPwzfojv7nCmSrOhOU96M_bsUYrU9yihw4MdddrFz_oUBKYRaHNunc3ppFqkhbhG6N4sKJ7Q9K_Iu7lyaXdEZ0kLLmknQtLfNeVPZyuqnRWGDMOes-tou1Lxgu-_9NwDhR8nwudBTdaPNGGkESftHyPYWX-iy91_zmvrVvcDQXyCzhNLKd5ZptZUh4Xg_VHiJeXZW5wSQV9F4vSAWqjAryGZ6Fb4NtGZklkoFP52F50I62whzttwpKGJ06va8-F0rrVQOL3YYsoWQKj9DEIGcWxhh3_SsWpoJD1Y87cvoYKEyWZxVRmNg6Ji3VXgPHrcAAHnAi7oGDNaP4nIAK8CWVuh4vaOjQ" alt="pickle rick"/>
+                                src="https://storage.googleapis.com/info343/pickle_rick.jpg" alt="pickle rick"/>
                         </div>
                 }
             </div>
