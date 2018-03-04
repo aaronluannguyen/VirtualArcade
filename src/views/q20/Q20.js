dece879a2372860a3d6fc0dfc675fda170af06e7
Kevin Xu <kevinx97@uw.edu>
2018-03-04 13:26:53 -0800

    rendering some views

--- src/views/q20/Q20.js
+++ src/views/q20/Q20.js
@@ -5,15 +5,13 @@ export default class Q20 extends React.Component{
         super(props);
         this.state = {
             api_url: 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBrTO7yPXtCN0iWkxeDKJGdc1ELDWOxs1M',
-            questions: 0
+            questionNum: 0,
+            questionString: "",
+            playing: false
         }
     }
     
-    handleResponse(response) {
-        console.log("getting cloud vision response");
-        console.log(response.json());
-    }
-    render(){
+    componentWillMount() {
         let req = {
             "requests":[
                     {
@@ -36,7 +34,28 @@ export default class Q20 extends React.Component{
             body: req,
             method: 'POST'
         })
-        .then(response => this.handleResponse(response));
-        return (<h1>Q20 Placeholder</h1>);
+        .then(response => this.handleResponse(response))
+        .then(data => console.log(data.responses[0].webDetection));
+    }
+    
+    handleResponse(response) {
+        console.log("getting cloud vision response");
+        return response.json();
+    }
+
+    render(){
+        
+        return (
+            <div className="container">
+                {
+                    playing ? 
+                        <div className="container picture">
+                            <img className="img-fluid" onClick={evt => this.setState({playing: true})}
+                                src="gs://info343/myFace.jpg" alt="pickle rick"/>
+                        </div> :
+                        <div className="container question"> </div>
+                }
+            </div>
+        );
     }
 }
