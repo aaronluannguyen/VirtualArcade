d226e9f0aca1c935d3bdc4e603aef78ca0f6b3eb
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-04 16:00:29 -0800

    css adjustments to tile to account for click event. Still waiting on game fixes and initiating grid

--- src/views/tictactoe/Tiles.js
+++ src/views/tictactoe/Tiles.js
@@ -4,13 +4,33 @@ import "./TicTacToe.css";
 export default class Tile extends React.Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            clicked: false,
+            gcontroller: this.props.gcontroller
+        }
+    }
+
+    action() {
+        this.setState({clicked: true});
+        //this.state.gcontroller.gbg.handleClick(this.props.xCoordinate, this.props.yCoordinate);
     }
 
     render() {
         return (
-            <button id="tttTile" className="btn btn-primary">
-               x: {this.props.xCoordinate} y: {this.props.yCoordinate}
-            </button>
+            <div id="tttTileContainer">
+                {
+                    this.state.clicked ?
+                        <button id="tttTile" className="btn btn-primary" onClick={() => this.action()}>
+                            CLICKED
+                            x: {this.props.xCoordinate} y: {this.props.yCoordinate}
+                        </button>
+                        :
+                        <button id="tttTile" className="btn btn-primary" onClick={() => this.action()}>
+                            x: {this.props.xCoordinate} y: {this.props.yCoordinate}
+                        </button>
+                }
+            </div>
         );
     }
 }
