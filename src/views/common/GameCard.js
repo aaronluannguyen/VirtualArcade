8536a75b85a30dc0f71ffa5e81a2d8a12227b1fd
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-06 01:28:40 -0800

    Set up main games for main page with appropraite pictures

--- src/views/common/GameCard.js
+++ src/views/common/GameCard.js
@@ -4,11 +4,27 @@ export default class TicTacToe extends React.Component{
     render(){
         return (
             <div className="card" id="game-card">
-                <img className="card-img-top" id="game-image" src={require(`./tictactoe.png`)} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">{this.props.gameName}</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
+                {
+                    this.props.gameName === "Tic-Tac-Toe" ?
+                        <img className="card-img-top" id="game-image" src={require(`./tictactoe.png`)} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">{this.props.gameName}</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        :
+                        this.props.gameName === "Connect 4" ?
+                            <img className="card-img-top" id="game-image" src={require(`./connect4.png`)} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">{this.props.gameName}</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            :
+                            <img className="card-img-top" id="game-image" src={require(`./Q20.png`)} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">{this.props.gameName}</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                }
             </div>
         );
     }
