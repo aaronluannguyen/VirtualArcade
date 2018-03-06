e33a7cec282a95c00070cfcd854936e35b3d4364
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-06 01:39:17 -0800

    Fixed syntax issues inside gamecard component

--- src/views/common/GameCard.js
+++ src/views/common/GameCard.js
@@ -6,23 +6,29 @@ export default class TicTacToe extends React.Component{
             <div className="card" id="game-card">
                 {
                     this.props.gameName === "Tic-Tac-Toe" ?
-                        <img className="card-img-top" id="game-image" src={require(`./tictactoe.png`)} alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">{this.props.gameName}</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div>
+                            <img className="card-img-top" id="game-image" src={require(`./tictactoe.png`)} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5>{this.props.gameName}</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
                         </div>
                         :
                         this.props.gameName === "Connect 4" ?
-                            <img className="card-img-top" id="game-image" src={require(`./connect4.png`)} alt="Card image cap" />
-                            <div className="card-body">
-                                <h5 className="card-title">{this.props.gameName}</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div>
+                                <img className="card-img-top" id="game-image" src={require(`./connect4.png`)} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{this.props.gameName}</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
                             </div>
                             :
-                            <img className="card-img-top" id="game-image" src={require(`./Q20.png`)} alt="Card image cap" />
-                            <div className="card-body">
-                                <h5 className="card-title">{this.props.gameName}</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div>
+                                <img className="card-img-top" id="game-image" src={require(`./Q20.png`)} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{this.props.gameName}</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
                             </div>
                 }
             </div>
