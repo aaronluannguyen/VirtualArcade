4547e1338194bcc381e3185eaaba867809ee41d5
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-06 22:53:06 -0800

    Finished styling player card on home page

--- src/views/common/PlayerCard.js
+++ src/views/common/PlayerCard.js
@@ -3,14 +3,10 @@ import React from "react";
 export default class TicTacToe extends React.Component{
     render(){
         return (
-            <div id="player-card" className="card text-white bg-dark mb-3">
-                <div class="card-header">
-                    <h1>Welcome!</h1>
-                </div>
-                <div class="card-body">
-                    <img id="player-picture" src={require(`./gamer.jpeg`)} alt="gamer profile picture"/>
-                    <h3>{this.props.player}</h3>
-                </div>
+            <div id="player-card">
+                <h3>Hello</h3>
+                <img id="player-picture" src={require(`./gamer.jpeg`)} alt="gamer profile picture"/>
+                <h3>{this.props.player}</h3>
             </div>
         );
     }
