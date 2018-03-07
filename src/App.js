b4ed4266390ed30043ce2bd47cac8418eccdd395
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-06 22:03:41 -0800

    More styling and added player card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import PlayerController from './controllers/common/PlayerController';
 import GameChooser from './views/common/GameChooser'
+import PlayerCard from "./views/common/PlayerCard";
 
 import TicTacToe from "./views/tictactoe/TicTacToe";
 import Connect4 from "./views/connect4/Connect4";
@@ -43,9 +44,7 @@ class App extends Component {
               <h1>The Virtual Arcade</h1>
           </div>
         </div>
-        <header className="App-header">
           <h1 className="App-title">{this.state.playerController ? <div>Welcome {this.state.playerController.getName() }!</div> : <div>Not yet logged In</div>}</h1>
-        </header>
         <main>
           <GameChooser playerInfo={this.state.playerController}/>
         </main>
