b37cb4581b08ff5b4435a07e968ffce5f53123b7
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-06 22:56:20 -0800

    Added more to player card

--- src/views/common/PlayerCard.js
+++ src/views/common/PlayerCard.js
@@ -4,9 +4,10 @@ export default class TicTacToe extends React.Component{
     render(){
         return (
             <div id="player-card">
-                <h3>Hello</h3>
+                <h3>Hello!</h3>
                 <img id="player-picture" src={require(`./gamer.jpeg`)} alt="gamer profile picture"/>
                 <h3>{this.props.player}</h3>
+                <p>(Your New Cool Gamer Name)</p>
             </div>
         );
     }
